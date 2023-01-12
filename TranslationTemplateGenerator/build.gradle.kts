165b797933db8643397e5ef80053be9b030019df
chr56 <_>
2023-01-12 16:49:57 +0800

    [Gradle] add `ShadowJar` to pack jar

--- TranslationTemplateGenerator/build.gradle.kts
+++ TranslationTemplateGenerator/build.gradle.kts
@@ -1,16 +1,30 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.7.21"
     kotlin("plugin.serialization") version "1.7.21"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 repositories {
     mavenCentral()
     google()
 }
 
-kotlin {
+tasks.named<ShadowJar>("shadowJar") {
+    archiveBaseName.set("TranslationTemplateGenerator")
+    archiveClassifier.set("")
+    manifest {
+        attributes["Main-Class"] = "com.unciv.utilities.TranslationTemplateGeneratorKt"
+    }
 }
 
-dependencies{
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     implementation("io.github.xn32:json5k:0.2.1") // json with comments support
 
